5409ff9b331f463420965e72e579ffe4d320ce36
Oleg Oshkoderov [TSC] <Oleg.Oshkoderov@tsceu.com>
2019-12-18 14:59:40 +0100

    Add my test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,49 @@
+/* eslint no-console: "off" */
 import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mount } from "@vue/test-utils";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).to.include(msg);
-  });
+const Component = {
+  template: "<p>{{ innerValue }}</p>",
+  props: {
+    value: String
+  },
+  data() {
+    return {
+      delayedValue: null,
+      innerValue: null
+    };
+  },
+  watch: {
+    value: {
+      immediate: true,
+      async handler(val) {
+        console.log(val);
+        this.delayedValue = val;
+        let text = "";
+        if (val) {
+          text = await Promise.resolve(val);
+        }
+
+        if (this.delayedValue == val) {
+          this.innerValue = text;
+        }
+      }
+    }
+  }
+};
+
+it.only("should update inner value", async () => {
+  const wrapper = mount(Component);
+  await wait(100);
+  console.log('setting props');
+  wrapper.setProps({ value: "good" });
+  console.log('props are set.');
+  await wait(100);
+  expect(wrapper.vm.innerValue).be.equal("good");
 });
+
+function wait(time) {
+  return new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
+}
